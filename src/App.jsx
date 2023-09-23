a60f4363431679ed0b74f92f9430b092fd97ce46
Am4rv <ariosf@utem.cl>
2023-09-22 22:09:43 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './app.css';
+import Header from './components/Header';
+import Layout from "./components/Layout"
+import Registro from './components/Registro/Registro';
+
+function App() {
+  return (
+    <>
+      <Layout>
+        <Registro/>
+      </Layout>
+    </>
+  );
+  
+}
+
+
+
+export default App
